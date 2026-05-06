0ec5eb52c13361a548ffb81e640d8e9a9e9945c1
Web3CyberServices <web3cyberservices@gmail.com>
2026-05-06 07:22:19 +0000

    The app isn't starting. Please investigate what could be wrong based on

--- next.config.ts
+++ next.config.ts
@@ -9,6 +9,9 @@ const nextConfig: NextConfig = {
   eslint: {
     ignoreDuringBuilds: true,
   },
+  experimental: {
+    allowedDevOrigins: ['*.cloudworkstations.dev'],
+  },
   images: {
     remotePatterns: [
       {
