a934a2fecf4649914c8cb470488f810b52329a7a
Web3CyberServices <web3cyberservices@gmail.com>
2026-07-07 16:52:13 +0000

    В прошлый раз мы руками удалили сломанный next.config.ts через терминал

--- next.config.mjs
+++ next.config.mjs
@@ -1,11 +1,30 @@
-
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: 'standalone',
-  reactStrictMode: true,
   images: {
-    unoptimized: true,
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'picsum.photos',
+      },
+    ],
   },
+  typescript: {
+    ignoreBuildErrors: true,
+  },
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
+  experimental: {
+    allowedDevOrigins: [
+      "localhost:3000",
+      "localhost:9002",
+      "0.0.0.0:3000",
+      "0.0.0.0:9002",
+      "*.cluster-4cmpbiopffe5oqk7tloeb2ltrk.cloudworkstations.dev",
+      "*.cloudworkstations.dev"
+    ]
+  }
 };
 
 export default nextConfig;
